f6970ad76230cc2deee2567902dc2ce04aad3a71
akshay <akshaysharma5432@gmail.com>
2023-08-01 15:30:53 -0400

    fix the search bar error

--- src/components/searchTask/SearchTask.tsx
+++ src/components/searchTask/SearchTask.tsx
@@ -1,19 +1,9 @@
-import { useState, useEffect } from "react";
-
 import "./searchTask.css";
 import { useTasks } from "../../context/TaskContext";
 
 const SearchTask = () => {
   //get tasks list, searchquery state, setQuery and addTask from TaskContext
-  const { tasks, searchQuery, setSearchQuery, addTask } = useTasks();
-
-  //hide and show search bar based on tasks are in the list or list is empty
-  const [visible, setVisible] = useState(false);
-
-  useEffect(() => {
-    //searchBar will hide and becomes visible based on the length of the tasks.
-    setVisible(tasks.length > 0);
-  }, [tasks]);
+  const { searchQuery, setSearchQuery, addTask } = useTasks();
 
   const handleQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
@@ -25,7 +15,6 @@ const SearchTask = () => {
   return (
     <div className="searchBarContainer">
       <input
-        className={`searchBar ${visible ? "showSearchBar" : ""}`}
         type="search"
         value={searchQuery}
         // on change of the input, if user types set the query to that value
