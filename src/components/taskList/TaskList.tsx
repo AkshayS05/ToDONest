b0767689cca3b02dc31508ed15b0b0ecab979cb4
akshay <akshaysharma5432@gmail.com>
2023-08-01 03:33:49 -0400

    dispaly the task list items

---
+++ src/components/taskList/TaskList.tsx
@@ -0,0 +1,21 @@
+import { useTasks } from "../../context/TaskContext";
+import Task from "../task/Task";
+// styles
+import "./taskList.css";
+
+const TaskList = () => {
+  //get the tasks list from the context hook.
+  const { tasks } = useTasks();
+  return (
+    <div>
+      <ul className="taskList">
+        {/* iterate over the tasks and create a list */}
+        {tasks?.map((task) => (
+          <Task key={task.id} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TaskList;
